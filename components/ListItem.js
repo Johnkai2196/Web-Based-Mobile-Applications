90053e4a692fc90e5c00f18b547cd71a290c9cd7
Johnkai Cortez <73104201+Johnkai2196@users.noreply.github.com>
2022-01-30 01:27:22 +0200

    edit a bit

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {uploadsUrl} from '../utils/variables';
-import {Avatar, ListItem as RNEListItem} from 'react-native-elements';
+import {Button, Card, Text} from 'react-native-elements';
 
 const ListItem = ({navigation, singleMedia}) => {
   return (
-    <RNEListItem
-      bottomDivider
-      onPress={() => {
-        navigation.navigate('Single', {file: singleMedia});
-      }}
-    >
-      <Avatar
+    <Card>
+      <Card.Title>{singleMedia.title}</Card.Title>
+      <Card.Image
         size="large"
-        square
         source={{uri: uploadsUrl + singleMedia.thumbnails.w160}}
-      ></Avatar>
-      <RNEListItem.Content>
-        <RNEListItem.Title numberOfLines={1} h4>
-          {singleMedia.title}
-        </RNEListItem.Title>
-        <RNEListItem.Subtitle numberOfLines={1}>
-          {singleMedia.description}
-        </RNEListItem.Subtitle>
-      </RNEListItem.Content>
-      <RNEListItem.Chevron />
-    </RNEListItem>
+      />
+      <Text
+        style={{
+          marginBottom: 10,
+          marginTop: 10,
+        }}
+      >
+        {singleMedia.description}
+      </Text>
+      <Button
+        onPress={() => {
+          navigation.navigate('Single', {file: singleMedia});
+        }}
+        title="Full screen"
+      ></Button>
+    </Card>
   );
 };
 

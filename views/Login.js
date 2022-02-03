465b854dc0ab90e99867a4e7628207887d73c4e4
Johnkai Cortez <73104201+Johnkai2196@users.noreply.github.com>
2022-02-03 11:44:12 +0200

    svg and extra

--- views/Login.js
+++ views/Login.js
@@ -14,6 +14,7 @@ import {useUser} from '../hooks/ApiHooks';
 import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
 import {ButtonGroup, Card, Text} from 'react-native-elements';
+import Logo from '../assets/logo.svg';
 
 const Login = ({navigation}) => {
   const [formToggle, setFormToggle] = useState(true);
@@ -52,7 +53,7 @@ const Login = ({navigation}) => {
         style={styles.container}
       >
         <View style={styles.appTitle}>
-          <Text>MyApp</Text>
+          <Logo style={styles.logo} />
         </View>
         <View style={styles.form}>
           <Card>
@@ -93,6 +94,9 @@ const styles = StyleSheet.create({
   },
   form: {
     flex: 8,
+  },
+  logo: {
+    marginTop: 20,
   },
 });
 
